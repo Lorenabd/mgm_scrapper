00b60e25fbd7778e00825abc190586bb525f1474
Lorenabd <105020653+Lorenabd@users.noreply.github.com>
2025-09-16 13:19:39 -0400

    Create setup_and_run_mgm.sh
    add setup and run file

---
+++ setup_and_run_mgm.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Clone repository using SHH if it is not already cloned
+if [ ! -d "mgm_scrapper" ]; then
+    echo "Cloning repository using SSH..."
+    git clone git@github.com:Lorenabd/mgm_scrapper.git
+fi
+
+# Change to repository directory
+cd mgm_scrapper
+
+# Create and activate a virtual environment (optional)
+if [ ! -d "venv" ]; then
+    echo "Creating virtual environment..."
+    python3 -m venv venv
+fi
+
+source venv/bin/activate  # On Linux/macOS
+
+echo "Installing dependencies from requirements.txt..."
+pip install --timeout=300 -r requirements.txt
+
+# Source the environment variables from env.sh
+echo "Setting environment variables from env.sh..."
+source env.sh 
+
+#Execute first script
+echo "Executing script..."
+python access_market.py
